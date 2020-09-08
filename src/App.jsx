08421f4e9466a83649db416c1ffc68724eaca1cc
rhaija <rana@disruptk.com>
2020-09-08 16:02:56 +0300

    prepare system

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+
+      </header>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;

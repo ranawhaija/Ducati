b82ee5516e4c334ce2f2b040613694e723ac36f9
rhaija <rana@disruptk.com>
2020-09-08 16:40:17 +0300

    add tailwind css framework to the system

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
+import './assets/styles/tailwind.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

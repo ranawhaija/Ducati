fd7b48ef200f090bb0095616cebc6cfd9adf4883
rhaija <rana@disruptk.com>
2020-09-10 13:29:52 +0300

    Format files and remove unnesacary comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,128 @@
-import React, {Component} from 'react';
-import Slider from './components/Slider/Slider';
-import motocyclesData from './data/Data';
-import './App.css';
-import worldWide from './assets/images/world-wide.webp';
+import React, { Component } from "react";
+import Slider from "./components/Slider/Slider";
+import motocyclesData from "./data/Data";
+import "./App.css";
+import worldWide from "./assets/images/world-wide.webp";
 
+/**
+ * Main component in the project
+ *
+ * @export
+ * @class App
+ * @extends {Component}
+ * @author RHaija
+ */
 export default class App extends Component {
-
   constructor() {
     super();
 
-this.state = {
-  isMenuHidden: null,
+    this.state = {
+      isMenuHidden: null,
     };
   }
 
-  toggle = ()=> {
+  /**
+   *Toggle the state of menu
+   *
+   * @memberof App
+   */
+  toggle = () => {
     const currentState = this.state.isMenuHidden;
-    if(currentState === null) {
+    if (currentState === null) {
       this.setState({ isMenuHidden: false });
       return;
     }
-    this.setState({ isMenuHidden: !currentState }); 
-}
+    this.setState({ isMenuHidden: !currentState });
+  };
 
-getClassName = ()=> {
-  return this.state.isMenuHidden===null?'' : (this.state.isMenuHidden ? 'hide' : 'show')
-}
+  /**
+   * Return the class name based on state condition
+   *
+   * @memberof App
+   */
+  getClassName = () => {
+    return this.state.isMenuHidden === null ? "" : (this.state.isMenuHidden ? "hide" : "show");
+  };
 
-    render() {
-      return (
-        <div className="app">
-          <header className="app-header relative py-4 px-8 lg:py-16 lg:px-32 flex flex-row justify-between items-center">
-            <div onClick={this.toggle} className="menu-icon-container">
-              <svg className="h-10 w-10 stroke-current text-white hover:text-red-600 lg:h-12 lg:w-12" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 6H20M4 12H20M4 18H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header relative py-4 px-8 lg:py-16 lg:px-32 flex flex-row justify-between items-center">
+          <div onClick={this.toggle} className="menu-icon-container">
+            <svg
+              className="h-10 w-10 stroke-current text-white hover:text-red-600 lg:h-12 lg:w-12"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 6H20M4 12H20M4 18H20"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="description-container flex flex-row items-center">
+            <img alt="ww" className="w-6 h-5 mx-2" src={worldWide} />
+            <div className="text text-white content-center text-xs lg:text-base">
+              INTERNATIONAL WEBSITE<span className="mx-1 lg:mx-4">|</span>MY
+              DUCATI{" "}
             </div>
-            <div className="description-container flex flex-row items-center">
-              <img alt="ww" className="w-6 h-5 mx-2" src={worldWide} />
-              <div className="text text-white content-center text-xs lg:text-base">INTERNATIONAL WEBSITE<span className="mx-1 lg:mx-4">|</span>MY DUCATI </div>
-            </div>
-    
-          </header>
-          <main>
-              <div className={`side-menu-container
+          </div>
+        </header>
+
+        <main>
+          {/* This should be separted in a single component if it was a real case */}
+          <div
+            className={`side-menu-container
                text-white fixed z-10 ${this.getClassName()}`}>
-                 <span className="text-red-600 absolute top-0 py-6 font-black text-3xl" >DUCATI</span>
-                 <div onClick={this.toggle} className="close px-5 flex justify-end hover:text-red-600">X</div>
-                 <label className="text-gray-600">Discover More</label>
-<div className="my-6"><a rel="noopener" className="hover:text-red-600" target="_blank" href="https://www.ducati.com/ww/en/bikes/monster/monster-797">Monster 797</a></div>
-<div className="my-6"><a rel="noopener" className="hover:text-red-600" target="_blank" href="https://www.ducati.com/ww/en/bikes/monster/monster-821">Monster 821</a></div>
-<div className="my-6"><a rel="noopener" className="hover:text-red-600" target="_blank" href="https://www.ducati.com/ww/en/bikes/monster/monster-1200">Monster 1200</a></div>
+            <span className="text-red-600 absolute top-0 py-6 font-black text-3xl">
+              DUCATI
+            </span>
+            <div
+              onClick={this.toggle}
+              className="close px-5 flex justify-end hover:text-red-600">
+              X
+            </div>
+            <label className="text-gray-600">Discover More</label>
+            <div className="my-6">
+              <a
+                rel="noopener noreferrer"
+                className="hover:text-red-600"
+                target="_blank"
+                href="https://www.ducati.com/ww/en/bikes/monster/monster-797"
+              >
+                Monster 797
+              </a>
+            </div>
+            <div className="my-6">
+              <a
+                rel="noopener noreferrer"
+                className="hover:text-red-600"
+                target="_blank"
+                href="https://www.ducati.com/ww/en/bikes/monster/monster-821"
+              >
+                Monster 821
+              </a>
+            </div>
+            <div className="my-6">
+              <a
+                rel="noopener noreferrer"
+                className="hover:text-red-600"
+                target="_blank"
+                href="https://www.ducati.com/ww/en/bikes/monster/monster-1200"
+              >
+                Monster 1200
+              </a>
+            </div>
+          </div>
 
+          <Slider slidersData={motocyclesData}></Slider>
+        </main>
 
-              </div>
-              <Slider slidersData={motocyclesData}></Slider>
-          </main>
-          <footer>
-    
-          </footer>
-          <div className={`overlay ${this.getClassName()}`}></div>
-        </div>
-      );
-    }
-    
-  } 
-
+        <div className={`overlay ${this.getClassName()}`}></div>
+      </div>
+    );
+  }
+}
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,132 +1,146 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './Slider.css';
-import ducatiLogo from '../../assets/images/ducati-logo.webp';
-import monester from '../../assets/images/Monster.webp';
-const pageIndicators = []; 
-let selectedItem = 0;
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Slider.css";
+import ducatiLogo from "../../assets/images/ducati-logo.webp";
+import monester from "../../assets/images/Monster.webp";
 
+/**
+ * Slider/ Carousel component
+ *
+ * @export
+ * @class Slider
+ * @extends {Component}
+ * @author RHaija
+ */
 export default class Slider extends Component {
   static propTypes = {
-    slidersData: PropTypes.array.isRequired
-  }
+    slidersData: PropTypes.array.isRequired,
+  };
 
   static defaultProps = {
     slidersData: [],
-  }
+  };
   constructor(props) {
     super(props);
 
-this.state = {
-  selectedItem: 0,
-      // length: landingData.length
+    this.state = {
+      selectedItem: 0,
     };
-    
   }
-   onIndicatorClick =(index) => {
-    console.log('here');
-    selectedItem = index;
-  
-   }
-   renderPageIndicator = (slidersData , oo)=> {
+
+  /**
+   * Render page indicators elemnts with selected functionality
+   *
+   * @memberof Slider
+   */
+  renderPageIndicator = (slidersData, oo) => {
     let arr = [];
-    slidersData.forEach((data,i) => {
-      return (
-        arr.push(<span key={`ind${i}`} style={{backgroundColor: data.color}} 
-         onClick={()=>{ this.setState({selectedItem: i})}}
-        className= {`indicator  shadow-md ${this.state.selectedItem === i ? 'selected' : ''}`}></span>)
+    slidersData.forEach((data, i) => {
+      return arr.push(
+        <span
+          key={`ind${i}`}
+          style={{ backgroundColor: data.color }}
+          onClick={() => {
+            this.setState({ selectedItem: i });
+          }}
+          className={`indicator  shadow-md ${
+            this.state.selectedItem === i ? "selected" : ""
+          }`}
+        ></span>
       );
     });
     return arr;
-  }
-  
-   renderSpecs = (specs) => {
+  };
+
+  /**
+   * Render the specs of the motocycle
+   *
+   * @memberof Slider
+   */
+  renderSpecs = (specs) => {
     let arr = [];
-    Object.keys(specs).forEach(key => {
-      return (
-        arr.push(
-          <div key={key} className="info mb-2 flex flex-col">
-            <label className="capitalize">{key}</label>
-            <span>{specs[key]}</span>
-          </div>)
+    Object.keys(specs).forEach((key) => {
+      return arr.push(
+        <div key={key} className="info mb-2 flex flex-col">
+          <label className="capitalize">{key}</label>
+          <span>{specs[key]}</span>
+        </div>
       );
-    })
+    });
     return arr;
-  }
+  };
+
+  /**
+   * Main function to render the sliders
+   *
+   * @memberof Slider
+   */
   renderSliders = (slidersData) => {
     let arr = [];
     slidersData.forEach((data, i) => {
-      return (
-        arr.push(<div  key={i}
-         className={`slider-data absolute flex flex-col items-center ${this.state.selectedItem === i ? 'selected' : ''}`}>
-           {/* opacity: this.state.selectedItem === i ? 1 : 0, */}
+      return arr.push(
+        <div
+          key={i}
+          className={`slider-data absolute flex flex-col items-center ${
+            this.state.selectedItem === i ? "selected" : ""
+          }`}
+        >
+          {/* opacity: this.state.selectedItem === i ? 1 : 0, */}
           <span className="moto-id text-white">{data.id}</span>
-          <img alt="moto image"  className="absolute" src={data.image} />
+          <img alt="moto" className="absolute" src={data.image} />
           <div className="data-box bg-white rounded flex flex-col">
             {this.renderSpecs(data.specs)}
           </div>
-          
-        </div>)
+        </div>
       );
     });
     return arr;
-  }
+  };
+
+  
   render() {
-    const { data } = this.props;
     return (
       <div className="slider-component-container absolute top-0 flex flex-col justify-center">
         <div className="fixed-part flex flex-col items-center">
-          <img alt="logo"  className="ducati-logo mx-1 my-4  lg:mx-2 lg:my-0 lg:w-25" src={ducatiLogo} />
-          <img alt="moto family" width="322" className="ducati-family mx-1 my-6 lg:mx-2 lg:mt-12" src={monester} />
+          <img
+            alt="logo"
+            className="ducati-logo mx-1 my-4  lg:mx-2 lg:my-0 lg:w-25"
+            src={ducatiLogo}
+          />
+          <img
+            alt="moto family"
+            width="322"
+            className="ducati-family mx-1 my-6 lg:mx-2 lg:mt-12"
+            src={monester}
+          />
         </div>
         <div className="slider-data-container relative">
           {this.renderSliders(this.props.slidersData)}
         </div>
-        <div  className="indicators-container absolute flex justify-center items-center">
+        <div className="indicators-container absolute flex justify-center items-center">
           {this.renderPageIndicator(this.props.slidersData)}
-          </div>
+        </div>
         <div className="arrows absolute flex justify-between">
-          <span onClick={()=>{ this.state.selectedItem === 0? this.setState({selectedItem: this.props.slidersData.length-1}):this.setState({selectedItem: this.state.selectedItem-1}) }}  className="prev"></span>
-          <span onClick={()=>{ this.state.selectedItem === this.props.slidersData.length-1? this.setState({selectedItem: 0}):this.setState({selectedItem: this.state.selectedItem+1}) }} className="next"></span>
-
+          <span
+            onClick={() => {
+              this.state.selectedItem === 0
+                ? this.setState({
+                    selectedItem: this.props.slidersData.length - 1,
+                  })
+                : this.setState({ selectedItem: this.state.selectedItem - 1 });
+            }}
+            className="prev"
+          ></span>
+          <span
+            onClick={() => {
+              this.state.selectedItem === this.props.slidersData.length - 1
+                ? this.setState({ selectedItem: 0 })
+                : this.setState({ selectedItem: this.state.selectedItem + 1 });
+            }}
+            className="next"
+          ></span>
         </div>
       </div>
-  
     );
   }
 }
-
-
-
-
-// const Slider = ({
-//   slidersData, selectedI
-// }) => {
-//   return (
-//     <div className="slider-component-container absolute top-0 flex flex-col justify-center">
-//       <div className="fixed-part flex flex-col items-center">
-//         <img width="110" className="ducati-logo mx-2" src={ducatiLogo} />
-//         <img width="322" className="ducati-logo mx-2 mt-12" src={monester} />
-//       </div>
-//       <div className="slider-data-container relative">
-//         {renderSliders(slidersData)}
-//         <div  className="indicators-container absolute flex justify-center items-center">
-//         {renderPageIndicator(slidersData, selectedI)}
-//         </div>
-//       </div>
-//     </div>
-
-//   );
-// }
-// Slider.propTypes = {
-//   slidersData: PropTypes.array.isRequired,
-//   selectedI: PropTypes.bool
-// };
-
-// Slider.defaultProps = {
-//   slidersData: {},
-//   selectedI: 0
-  
-// };
-
-// export default Slider;
